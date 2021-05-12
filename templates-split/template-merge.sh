9548c37141b60a3ce42491ce5a447ec2006039b7
Mike Straney <blogpdq@blogpdq.com>
2021-05-12 00:13:13 -0400

    split up template v2

--- templates-split/template-merge.sh
+++ templates-split/template-merge.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-#mv templates.json templates.json.bu
-cat module* | sed 's@^//\s*\S*$@@g' | tr -s '\n' > templates-out.json
+
+## BEG of FILE
+echo '{' > templates-out.json
+echo '        "version": "2",' >> templates-out.json
+echo '        "templates": [' >> templates-out.json
+
+## Combine modules
+cat module* | sed 's@^//\s*\S*$@@g' | tr -s '\n' >> templates-out.json
+
+
+##END OF FILE
+echo '        ]' >> templates-out.json
+echo '}' >> templates-out.json
+
 exit
--- templates-split/template-split.sh
+++ templates-split/template-split.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-csplit --digits=3  --quiet --prefix=module templates.json "/\/\/ _________/+1" "{*}" && \
-for i in module*; do mv $i $(printf '%s' $i && cat $i | head -n 1 | sed 's/\/\/ /_/g'); done
+csplit --digits=3 --prefix=module templates.json "/^                },/+1" {*}
+cat module000 | tail -n +4 > aaTmp && mv aaTmp module000
+lastmod=module$(($(ls module* | wc -l) - 1)) && \
+cat $lastmod | head -n -2 > aaTmp && mv aaTmp $lastmod
+for i in module*; do mv $i $(printf '%s' $i"_" && cat $i | grep '"title":' | tr "'" "|" | sed -e "s/\s*.title.: //g" -e 's/[", /|]//g'); done
 exit
